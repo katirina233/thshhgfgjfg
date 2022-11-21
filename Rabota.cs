b3794db180c5a3caf7745f87dc59473168ddb123
katirina233 <118421008+katirina233@users.noreply.github.com>
2022-11-21 16:33:38 +0900

    Add files via upload

---
+++ Rabota.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.InteropServices;
+using System;
+
+namespace Dll_prac
+{
+    public class Rabota
+    {
+        string name;
+        public Rabota(string name)
+        {
+            this.name = name;
+        }
+        public void Print() => Console.WriteLine($"Name: {name}");
+    }
+}
